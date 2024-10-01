9875cdb3fe6e4f4584066d212fc634320f73e094
Ren <s0985514623@gmail.com>
2024-10-01 17:31:18 +0800

    feat:release name & Author name

--- plugin.php
+++ plugin.php
@@ -6,7 +6,7 @@
  * Version:           1.0.2
  * Requires at least: 5.7
  * Requires PHP:      8.0
- * Author:            Your Name
+ * Author:            Ren
  * Author URI:        https://github.com/s0985514623
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
